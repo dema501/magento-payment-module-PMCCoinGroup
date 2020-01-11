f53d7b9e31b045426864edb9d2037576756587ea
Dmitry Bashlov <dmitry.bashlov@theagilehub.net>
2020-01-11 11:15:26 -0600

    Standard template

--- app/code/local/Liftmode/PMCCoinGroup/Block/Form/Cc.php
+++ app/code/local/Liftmode/PMCCoinGroup/Block/Form/Cc.php
@@ -15,7 +15,7 @@ class Liftmode_PMCCoinGroup_Block_Form_Cc extends Mage_Payment_Block_Form_Cc
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('pmccoingroup/form/cc.phtml');
+        $this->setTemplate('payment/form/cc.phtml');
     }
 
 }
